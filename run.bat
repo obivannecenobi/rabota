8fdb774a07cf7b8170a26c1da17773a787b36200
obivannecenobi <obivan.necenobi@gmail.com>
2025-08-27 01:52:36 +0700

    feat: flatten project structure

--- run.bat
+++ run.bat
@@ -1,6 +1,7 @@
 @echo off
 REM === Create/Activate venv and run app ===
 setlocal enabledelayedexpansion
+cd /d "%~dp0"
 
 if not exist .venv (
     py -3 -m venv .venv
@@ -11,3 +12,4 @@ python -m pip install --upgrade pip
 pip install -r requirements.txt
 
 python -m app.main
+
